784c2d7183823418f34f1d11c1e7702cd12ffcc4
杨潇 <1838521688@qq.com>
2020-10-01 20:23:15 +0800

    Early version

--- src/components/UpLoadPic.vue
+++ src/components/UpLoadPic.vue
@@ -1,37 +1,16 @@
 <template>
-  <canvas id="QRCode" ref="QRCode" :width="size" :height="size">
+  <canvas id="QRCode" ref="QRCode" :data ="data" :width="size" :height="size">
   </canvas>
 </template>
 
 <script>
+
     export default {
         name: "UpLoadPic",
       props: {
         data: {
           type: String,
-          default:  "11111110000000001111111" +
-                    "10000010000000001000001" +
-                    "10111010000000001011101" +
-                    "10111010000000001011101" +
-                    "10111010000000001011101" +
-                    "10000010000000001000001" +
-                    "11111110000000001111111" +
-                    "00000000000000000000000" +
-                    "00000000000000000000000" +
-                    "00000000000000000000000" +
-                    "00000000000000000000000" +
-                    "00000000000000000000000" +
-                    "00000000000000000000000" +
-                    "00000000000000000000000" +
-                    "00000000000000000000000" +
-                    "00000000000000000000000" +
-                    "11111110000000001111100" +
-                    "10000010000000001000100" +
-                    "10111010000000001010100" +
-                    "10111010000000001000100" +
-                    "10111010000000001111100" +
-                    "10000010000000000000000" +
-                    "11111110000000000000000"
+          default:  ""
         },
         size: {
           type: String,
@@ -43,18 +22,26 @@
       },
       methods: {
           showOn(data) {
+            let map = [];
             let length = data.length;
             let amount = Math.sqrt(length);
+            for (let i = 0; i < length; i++){
+              map[i % amount] = map[i % amount] || [];
+              map[i % amount][Math.floor(i / amount)] = data.substr(i, 1) !== "0";
+            }
             let canvas = this.$refs.QRCode;
             let w = canvas.width;
             let h = canvas.height;
             let dw = w / amount, dh = h / amount;
             let ctx = canvas.getContext("2d");
             ctx.clearRect(0, 0, w, h);
+            let x = 0, y = 0;
             for (let i = 0; i < length; i++){
-              if (data.substr(i, 1) !== "0"){
+              x = i % amount;
+              y = Math.floor(i / amount);
+              if (map[x][y]){
                 ctx.fillStyle = "rgba(0, 0, 0, 1)";
-                ctx.fillRect((i % amount) * dw, Math.floor(i / amount) * dh, dw, dh);
+                ctx.fillRect(x * dw, y * dh, dw, dh);
               }
             }
           }
