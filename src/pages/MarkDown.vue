73d7c0c3d03745f1e77e7d64376e3ce8d205eb6e
杨潇 <1838521688@qq.com>
2020-10-25 15:15:02 +0800

    第一次修改

--- src/pages/MarkDown.vue
+++ src/pages/MarkDown.vue
@@ -2,16 +2,18 @@
   <div>
     <textarea :rows="10" v-model="text" @change="convertText"></textarea>
     <textarea :rows="10" v-model="html"></textarea>
-    <div>
+    <div id="d"
+         contenteditable="true"
+         class="text"
+         style="min-width: 10px; min-height: 300px;"
+         @input="convertHtml">
       <el-row>
         <quill-editor
-          id="d"
-          contenteditable="true"
           v-model = "content"
           :options="editorOption"
           @blur="onEditorBlur($event)"
           @focus="onEditorFocus($event)"
-          @change="convertHtml($event)">
+          @change="onEditorChange($event)">
         </quill-editor>
       </el-row>
     </div>
